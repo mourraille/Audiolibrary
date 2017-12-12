e730d0090875b760133f09787ff96a72507305b9
Mauricio Mourraille <mourraille@me.com>
2017-12-12 05:02:27 +0000

    fml

--- scanner.py
+++ scanner.py
@@ -1,4 +1,3 @@
-
 import RPi.GPIO as GPIO
 import time
 import serial
@@ -8,11 +7,22 @@ import os.path
 ser = ""
 ID = ""
 flag = False
-
+green = 23
+blue = 21
+red = 22
+scannable = True
+wasrecording = False
 def setup():
 
   GPIO.setmode(GPIO.BCM) # Numbers GPIOs by physical location
-  os.system('amixer set PCM -- 100%')
+  GPIO.setwarnings(False)
+  GPIO.setup(green, GPIO.OUT)
+  GPIO.setup(red, GPIO.OUT)
+  GPIO.setup(blue, GPIO.OUT)
+  GPIO.output(red, 0)
+  GPIO.output(green, 0)
+  GPIO.output(blue, 1)
+
   print "*************************** \nIniciando AUDIOTECA DIGITAL 1.0 \n"
 
 def start():
@@ -27,14 +37,17 @@ def start():
 
 
 def scan ():
+	global wasrecording
+	global scannable
 	global flag
         global ID
 	key = "0E0006989A0A"
-	while True:
+	while True and scannable:
 	    ser = serial.Serial('/dev/ttyAMA0',9600,timeout=0.20)
 	    read_byte = ser.read()
             if read_byte=="\x02":
-              os.system('killall -9 aplay')
+              GPIO.output(green, 1)
+	      os.system('killall -9 aplay')
 	      flag = False
               for i in range(12):
                 read_byte = ser.read()
@@ -42,26 +55,60 @@ def scan ():
               print "Code: "+ ID
               ser.close()
 	      if (ID == key):
+		if (wasrecording):
+		   os.system('killall -9 arecord')
+                   wasrecording = False
+ 		   GPIO.output(red,0)
+		GPIO.output(green,0)
+                GPIO.output(red,1)
 	        record()
 		ID = ""
 	      if (os.path.isfile(ID + '.wav')):
 		flag = True
+                GPIO.output(green,0)
 	      else:
                 ID = ""
-              time.sleep(0.8)
+              GPIO.output(green,0)
 
 def destroy():
    print ""
 
 def record():
-	print "recording"
+            key = "0E0006989A0A"
+	    name = ""
+            global scannable
+	    global wasrecording
+	    scannable = False
+	    while not scannable:
+             ser = serial.Serial('/dev/ttyAMA0',9600,timeout=0.20)
+             read_byte = ser.read()
+             if read_byte=="\x02":
+               for i in range(12):
+                 read_byte = ser.read()
+                 name = name + str(read_byte)
+               print "name: "+ name
+               ser.close()
+	       scannable = True
+               if (name != key and not os.path.isfile(name + '.wav')):
+	         print ("recording")
+	         wasrecording = True
+	         os.system ('arecord --device=hw:1,0 --format S16_LE --rate 44100 ' + name + '.wav' + ' -V mono')
+	         print "recording"
+ 	         GPIO.output(red,0)
+            name = ""
+            GPIO.output(red,0)
+	    return
 
-if __name__ == '__main__': # Program start from here
+if __name__ == '__main__':
   setup()
   try:
     sc = threading.Thread(target=scan, )
     sc.start()
+    GPIO.output(red,1)
+    time.sleep(0.5)
+    GPIO.output(red,0)
     start()
   except Exception as e:
+   GPIO.cleanup()
    print (e)
 
